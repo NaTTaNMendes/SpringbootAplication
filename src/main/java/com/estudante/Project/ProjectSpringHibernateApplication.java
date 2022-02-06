136f94cc89e7bec895c035c72c7079bebdbf2406
Nattan <nattan078@gmail.com>
2022-02-06 15:33:21 -0300

    Creating project

---
+++ src/main/java/com/estudante/Project/ProjectSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.estudante.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudante/Project/ProjectSpringHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudante.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
