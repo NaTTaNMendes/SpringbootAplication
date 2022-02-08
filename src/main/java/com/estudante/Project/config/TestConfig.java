273606c8f3a7ca72ebbcc11793a45409c3408d99
Nattan <nattan078@gmail.com>
2022-02-08 11:13:45 -0300

    Associação um para um no payment

--- src/main/java/com/estudante/Project/config/TestConfig.java
+++ src/main/java/com/estudante/Project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudante.Project.entities.Category;
 import com.estudante.Project.entities.Order;
 import com.estudante.Project.entities.OrderItem;
+import com.estudante.Project.entities.Payment;
 import com.estudante.Project.entities.Product;
 import com.estudante.Project.entities.User;
 import com.estudante.Project.entities.enums.OrderStatus;
@@ -76,5 +77,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-	}	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+	}
 }
